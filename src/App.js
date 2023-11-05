db3c44b9d97c1d9532b6bfad12f76ed9c5600d15
Er. Udhaya <46084161+udhaya09@users.noreply.github.com>
2023-11-05 13:41:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Wakeup at 4:30</td>
+                <td>Udhaya</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Excercise till 5:30</td>
+                <td>Udhaya</td>
+              </tr>
+              <tr>
+                <th scope='row'>3</th>
+                <td>Walking till 7:00</td>
+                <td>Udhaya</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
